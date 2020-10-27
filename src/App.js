e7e8330e03029ccf45886c3e96bb6266b1cb7c68
boldCM <carolin@cc-meier.de>
2020-10-27 21:25:24 +0100

    Add some styling

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import ImagePreview from "./components/ImagePreview";
 function App() {
   return (
     <main>
-      <button>Get Random Image</button>
+      <button className="btn">Get Random Image</button>
       <ImagePreview
         src={
           "https://images.unsplash.com/photo-1603614486387-276f74fcbe2a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1947&q=80"
