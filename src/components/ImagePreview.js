d7fa9ffaa2d9f89f424887ca33a6937100fdefc8
boldCM <carolin@cc-meier.de>
2020-10-28 12:05:58 +0100

    Create favourites-btn

--- src/components/ImagePreview.js
+++ src/components/ImagePreview.js
@@ -4,6 +4,7 @@ import "./ImagePreview.css";
 export default function ImagePreview({ src, alt, author }) {
   return (
     <div className="imageContainer">
+      <button className="btn__favourites">❤</button>
       <img className="imageThumb" src={src} alt={alt} />
       <p className="imageAuthor"> Author: {author}</p>
     </div>
